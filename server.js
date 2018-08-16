a4410345bd13a27b9f0642b3608201e9e5f59b64
igor <igor@c-log.io>
2018-08-16 13:23:50 +0200

    new folder structure

--- server.js
+++ server.js
@@ -1,55 +1,23 @@
 
-var http = require('http');
-var fs = require('fs');
-var path = require('path');
+var express = require('express'),
+htmlDir = './html/'
+var app = express();
 
-http.createServer(function (request, response) {
-    console.log('request ', request.url);
+//Log all requests
+//app.use(express.logger());
 
-    var filePath = '.' + request.url;
-    if (filePath == './') {
-        filePath = './index.html';
-    }
+//Set content directories
+app.use(express.static(__dirname + '/html'));
+app.use('/js',express.static(__dirname + '/js'));
+app.use('/css', express.static(__dirname + '/css'));
+app.use('/fonts', express.static(__dirname + '/fonts'));
+app.use("/image", express.static(__dirname + '/image'));
 
-    var extname = String(path.extname(filePath)).toLowerCase();
-    var mimeTypes = {
-        '.html': 'text/html',
-        '.js': 'text/javascript',
-        '.css': 'text/css',
-        '.json': 'application/json',
-        '.png': 'image/png',
-        '.jpg': 'image/jpg',
-        '.gif': 'image/gif',
-        '.wav': 'audio/wav',
-        '.mp4': 'video/mp4',
-        '.woff': 'application/font-woff',
-        '.ttf': 'application/font-ttf',
-        '.eot': 'application/vnd.ms-fontobject',
-        '.otf': 'application/font-otf',
-        '.svg': 'application/image/svg+xml'
-    };
+app.get('/', function(request, response) {
+response.sendFile(htmlDir + '/index.html');
+});
 
-    var contentType = mimeTypes[extname] || 'application/octet-stream';
-
-    fs.readFile(filePath, function(error, content) {
-        if (error) {
-            if(error.code == 'ENOENT') {
-                fs.readFile('./404.html', function(error, content) {
-                    response.writeHead(200, { 'Content-Type': contentType });
-                    response.end(content, 'utf-8');
-                });
-            }
-            else {
-                response.writeHead(500);
-                response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
-                response.end();
-            }
-        }
-        else {
-            response.writeHead(200, { 'Content-Type': contentType });
-            response.end(content, 'utf-8');
-        }
-    });
-
-}).listen(8080);
-console.log('Server running at http://127.0.0.1:8080/');
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+console.log("Listening on " + port);
+});
